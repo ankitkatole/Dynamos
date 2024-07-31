9df8f11ffdd89d2c8697f5e96e862e82c11f8ee8
Ankit Katole <148646317+ankitkatole@users.noreply.github.com>
2024-07-31 19:43:30 +0530

    Delete server directory

--- server/src/controllers/root.js
+++
@@ -1,6 +0,0 @@
-
-const root = async (req,res) => {
-    res.send("Welcome to Digiwell Server");
-};
-
-module.exports = root
--- server/src/routes/contactUs.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const contactUs = require('../controllers/contactUs');
-
-const router = express.Router();
-
-router.post("/",contactUs);
-
-module.exports = router;
